724ee4369e5c02d02f48522ae9daa6a03df956ba
ValetGames-Coder <valter41.games@gmail.com>
2025-04-01 21:35:36 +0300

    some upd

--- include/scenes/main_menu_scene.hpp
+++ include/scenes/main_menu_scene.hpp
@@ -9,6 +9,7 @@
 class MainMenuScene : public Scene
 {
 private:
+    static bool intro;
     unsigned short selected;
     std::vector<std::string> options {"Player vs Player", "Player vs Bot", "Rules", "Exit"};
 public:
--- src/scenes/main_menu_scene.cpp
+++ src/scenes/main_menu_scene.cpp
@@ -1,6 +1,8 @@
 #include "scenes/main_menu_scene.hpp"
 #include "animation/animator.hpp"
 
+bool MainMenuScene::intro = false;
+
 void MainMenuScene::load()
 {
     curs_set(0);
@@ -10,10 +12,16 @@ void MainMenuScene::load()
     Position startShipPosition {(short)(yMax / 2 - shipSize.y / 2 - logoSize.y / 2), (short)(xMax - shipSize.x)};
     Position endShipPosition {(short)(yMax / 2 - shipSize.y / 2 - logoSize.y / 2), (short)(xMax / 2 - shipSize.x / 2)};
     
-    Animation::move(sceneWin, startShipPosition, endShipPosition, "./img/ship.txt", 1, COLOR_CYAN);
+    if (intro == false)
+    {
+        Animation::move(sceneWin, startShipPosition, endShipPosition, "./img/ship.txt", 1, COLOR_CYAN);
+        intro = true;
+    }
+    else
+        printImage(sceneWin, endShipPosition - Position(1, 0), "./img/ship.txt", COLOR_CYAN);
     printImage(sceneWin, logoPosition, "./img/logo.txt", COLOR_CYAN);
     //printf("Ship count here is %i and %i df-!d,{}{SScf}sdouty");
-    Position panelPosition {(short)(yMax / 2 + (logoSize.y + shipSize.y) / 2), (short)(xMax / 2)};
+    Position panelPosition {(short)(yMax / 2 + (logoSize.y + shipSize.y) / 2), (short)((xMax / 2) - 2)};
     Panel* panel = new Panel(panelPosition, options);
     selected = panel->getSelectedItem();
 }
